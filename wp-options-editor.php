3ba53bc57cee114950600acfd744b69651b96e1e
Christian Foellmann <foellmann@foe-services.de>
2018-10-01 11:08:00 +0200

    limit require to is_admin

--- wp-options-editor.php
+++ wp-options-editor.php
@@ -10,10 +10,8 @@ Text Domain: wp-options-editor
 Domain Path: /languages
 */
 
-// include the settings page.
-require_once( 'admin/manager-page.php' );
-
 // Load the settings page if we're in the admin section.
 if ( is_admin() ) {
+	require_once( 'admin/manager-page.php' );
 	$settings = new OptionsManagerSettingsPage( __FILE__ );
 }
